9234cafc1ec0b48825934fe2446447ae237c4985
R7Nick7A <Maugly359@yandex.ru>
2025-04-24 20:42:42 +0500

    Базовый код

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import './scss/styles.scss';
+import { TestComponent } from './components/base/Component';
+
+const app = document.getElementById('app');
+if (app) {
+  const counter = new TestComponent();
+
+  let count = 0;
+  counter.setClickHandler(() => {
+    count++;
+    counter.updateText();
+  });
+
+  app.append(counter.render());
+}
